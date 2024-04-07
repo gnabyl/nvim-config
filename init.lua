3fe992f855bd3a5d1c3e6469fa147b64bb3c4d5f
Bang NGUYEN <lelybang.nguyen@gmail.com>
2024-04-07 17:06:51 +0200

    set tab size

--- init.lua
+++ init.lua
@@ -789,5 +789,8 @@ require('lazy').setup({
   },
 })
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
