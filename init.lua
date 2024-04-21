42809f6cbb9c85dd6290d7a6ee3037f5a65e15a3
Bang NGUYEN <lelybang.nguyen@gmail.com>
2024-04-21 13:44:12 +0200

    move set to custom

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-require 'set'
+require 'custom.set'
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
